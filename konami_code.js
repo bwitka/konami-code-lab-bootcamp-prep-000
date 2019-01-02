32f0beccf532d013557e3215b7d5fc6247699633
BEAU WITKA <bwitka@gmail.com>
2019-01-02 01:56:01 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,5 +14,19 @@ const codes = [
 function init() {
   let index = 0;
   
-  
+  function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 }
